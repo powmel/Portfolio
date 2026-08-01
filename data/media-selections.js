dde368eab817678dc0d301c6c123c5c36f27bb08
Taiki Misawa <taiki.msw@gmail.com>
2026-08-01 23:37:40 +0900

    daily: publish August 1 reflection and approved photos

--- data/media-selections.js
+++ data/media-selections.js
@@ -1,1 +1,42 @@
-window.MEDIA_SELECTIONS = {};
+window.MEDIA_SELECTIONS = {
+  "2026-07-26": [
+    {
+      "mediaId": "6bf7ded2e3136986",
+      "src": "images/daily/2026-07-26/6bf7ded2e3136986.jpg",
+      "capturedAt": "2026-07-26T12:04:36Z",
+      "alt": "2026-07-26の記録写真"
+    },
+    {
+      "mediaId": "6d40dae8c70fd4c8",
+      "src": "images/daily/2026-07-26/6d40dae8c70fd4c8.jpg",
+      "capturedAt": "2026-07-26T12:04:14Z",
+      "alt": "2026-07-26の記録写真"
+    },
+    {
+      "mediaId": "041a4bb9eac96891",
+      "src": "images/daily/2026-07-26/041a4bb9eac96891.jpg",
+      "capturedAt": "2026-07-26T07:49:02Z",
+      "alt": "2026-07-26の記録写真"
+    },
+    {
+      "mediaId": "486f035bd2773c04",
+      "src": "images/daily/2026-07-26/486f035bd2773c04.jpg",
+      "capturedAt": "2026-07-26T07:31:33Z",
+      "alt": "2026-07-26の記録写真"
+    }
+  ],
+  "2026-07-25": [
+    {
+      "mediaId": "7b47c940caa0c004",
+      "src": "images/daily/2026-07-25/7b47c940caa0c004.jpg",
+      "capturedAt": "2026-07-25T18:09:41Z",
+      "alt": "2026-07-25の記録写真"
+    },
+    {
+      "mediaId": "70b452bd0e2e26c4",
+      "src": "images/daily/2026-07-25/70b452bd0e2e26c4.jpg",
+      "capturedAt": "2026-07-25T18:08:49Z",
+      "alt": "2026-07-25の記録写真"
+    }
+  ]
+};
